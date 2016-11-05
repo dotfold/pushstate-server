0232278a46c26cc6715e39424b0fd36e26aa2e24
dotfold <jamesmcnamee@gmail.com>
2016-11-05 22:25:11 +1100

    migrate to express, change middleware order

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var connect = require('connect')
+var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var serveStaticFile = require('connect-static-file')
 var compression = require('compression')
-var app = connect()
+var app = express()
 var httpProxy = require('http-proxy-middleware')
 
 const PORT = 9000
@@ -15,12 +15,20 @@ const FILE = 'index.html'
 exports.start = function (options, _onStarted) {
   options = options || {}
 
-  let port = options.port || process.env.PORT || PORT
-  let directory = options.directory || DIRECTORY
-  let directories = options.directories || [directory]
-  let file = options.file || FILE
-  let proxy = options.proxy
-  let onStarted = _onStarted || function () {}
+  const port = options.port || process.env.PORT || PORT
+  const directory = options.directory || DIRECTORY
+  const directories = options.directories || [directory]
+  const file = options.file || FILE
+  const proxy = options.proxy
+  const onStarted = _onStarted || function () {}
+
+  const proxyOptions = {
+    target: proxy,
+    logLevel: 'debug',
+    onError: function (proxy) { console.log(proxy) },
+    secure: false,
+    changeOrigin: true
+  }
 
   app.use(compression())
 
@@ -29,23 +37,16 @@ exports.start = function (options, _onStarted) {
     app.use(serveStatic(directory, { extensions: ['html'] }))
   })
 
-  // Then, serve the fallback file
-  app.use(serveStaticFile(path.join(directory, file)))
-
+  // Check if the request matches the proxy string, if used
   if (proxy && typeof proxy === 'string') {
     var mayProxy = /^(?!\/(index\.html$)).*$/
     app.use(mayProxy,
-      // Pass the scope regex both to Express and to the middleware for proxying
-      // of both HTTP and WebSockets to work without false positives.
-      httpProxy(pathname => mayProxy.test(pathname), {
-        target: proxy,
-        logLevel: 'debug',
-        onError: function (proxy) { console.log(proxy) },
-        secure: false,
-        changeOrigin: true
-      })
+      httpProxy(pathname => mayProxy.test(pathname), proxyOptions)
     )
   }
+
+  // Then, serve the fallback file
+  app.use(serveStaticFile(path.join(directory, file)))
 
   return app.listen(port, function (err) {
     onStarted(err, port)
